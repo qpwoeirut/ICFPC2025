71251a468803e68fa714d526e80fd8364a246e36
qpwoeirut <stanleyzhong8@gmail.com>
2025-09-06 17:29:27 -0700

    Add addendum problem names

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ config = dotenv.dotenv_values()
 BASE_URL = "https://31pwr5t6ij.execute-api.eu-west-2.amazonaws.com"
 TEAM_ID = config["TEAM_ID"]
 
+# All problem sets allow charcoal marking.
+# Problem set 1 has a route limit of 18n doors.
+# Problem sets 2 and 3 have a route limit of 6n doors.
 PROBLEMS = {3: "probatio", 6: "primus", 12: "secundus", 18: "tertius", 24: "quartus", 30: "quintus"}
+PROBLEMS_2 = {12: "aleph", 24: "beth", 36: "gimel", 48: "daleth", 60: "he"}
+PROBLEMS_3 = {18: "vau", 36: "zain", 54: "hhet", 72: "teth", 90: "iod"}
 
 
 def register(name: str, pl: str, email: str):
